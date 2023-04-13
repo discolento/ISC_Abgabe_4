ca6bc0ec1780e70fd00389c5016badbe26866dd9
Iris <127310038+discolento@users.noreply.github.com>
2023-04-13 21:59:51 +0200

    Abgabe 4
    final implementation of abgabe 4
    extended the detail view with description and accurate mash temperatures and times

--- main.js
+++ main.js
@@ -4,21 +4,20 @@ displayBeer()
 
 // Diese Funktion wird ausgeführt, sobald die Seite geladen wurde
 function displayBeer() {
-    // Die URL der API, die die Biere liefert
-    const url = `https://api.punkapi.com/v2/beers`;
+    const beerContainer = document.getElementById("beerContainer")
+    const url = "https://api.punkapi.com/v2/beers"; // Die URL der API, die die Biere liefert
 
-    // Abrufen der Daten von der API mit fetch
-        // ToDo
-
-    // Speichern der vollständigen Liste der Biere in das vorgegebene Array
-        // ToDo
-
-    // Generieren des HTML-Codes für jedes Bier und Zusammenfügen in eine einzige Zeichenkette => Hilfestellung: data.map(generateBeerHtml).join('')
-        // ToDo
-
-    // Fügen des HTML-Codes zur beerContainer-Div-Box hinzu
-        // ToDo
+    fetch(url) // Abrufen der Daten von der API mit fetch
+        .then(response => response.json())
+        .then(data => {
+            fullBeerList = data; // Speichern der vollständigen Liste der Biere in das vorgegebene Array
+            const beerHtml = data.map(generateBeerHtml).join('');//  Generieren des HTML-Codes für jedes Bier und Zusammenfügen in eine einzige Zeichenkette => Hilfestellung: data.map(generateBeerHtml).join('')
+            beerContainer.innerHTML = beerHtml;    // Fügen des HTML-Codes zur beerContainer-Div-Box hinzu
+            console.log(fullBeerList);
+        })
+        .catch(error => console.log(error))
 }
+
 
 /* Diese Funktion generiert den HTML-Code für ein einzelnes Bier
    Ersetze die Angaben <!--ToDo--> mit den korrekten Werten ${}
@@ -31,15 +30,15 @@ function displayBeer() {
 const generateBeerHtml = beer => `
   <section class="col">
     <div class="card">
-        <img src="<!--ToDo-->" class="card-img-top">
+        <img src="${beer.image_url}" class="card-img-top">
         <div class="card-body">
-            <h5 class="card-title"><!--ToDo--></h5>
+            <h5 class="card-title">${beer.name}</h5>
             <div class="card-text">
-                <p><!--ToDo--></p>
+                <p>${beer.tagline}</p>
             </div>
         </div>
         <div class="card-footer">
-          <a class="btn btn-dark btn-sm details-link" data-beer-id="<!--ToDo-->">weitere Details</a>
+          <a class="btn btn-dark btn-sm details-link" data-beer-id="${beer.id}">weitere Details</a>
         </div>
     </div>
   </section>
@@ -47,21 +46,22 @@ const generateBeerHtml = beer => `
 
 // Event-Listener für Details-Links
 document.addEventListener('click', event => {
-    // Überprüfen, ob das geklickte Element ein Details-Link ist
-    // ToDo
+    if (event.target.matches('.details-link')){// Überprüfen, ob das geklickte Element ein Details-Link ist
+        event.preventDefault();// ToDo
+        const beerId = event.target.getAttribute('data-beer-id') // Speichern sie die data-beer-id in eine Konstante und rufen sie die
+        getBeerDetails(beerId); // Funktion getBeerDetails mit der Konstante als Übergabeparameter auf
 
-        /* Speichern sie die data-beer-id in eine Konstante und rufen sie die
-         Funktion getBeerDetails mit der Konstante als Übergabeparameter auf */
-        // ToDo
-
+    };
 });
 
 // Diese Funktion zeigt die Details eines Biers in einem Modal-Fenster an
-function getBeerDetails(beerID) {
-    /* Speichern Sie das Objekt aus der fullBeerList an der Stelle der ID die als Übergabeparameter
+function getBeerDetails(beerId) {
+    const beer = fullBeerList.find(beer => {
+        console.log(beer.id, "=", beerId, beer.id === beerId, beer.id == beerId)
+        return beer.id === +beerId
+    });/* Speichern Sie das Objekt aus der fullBeerList an der Stelle der ID die als Übergabeparameter
        übergeben wird in die const beer die Sie hier definieren */
-    // ToDo
-
+    console.log(beer)// ToDo
     /* Das HTML-Code für das Modal-Fenster mit den Details des Biers
        Fügen Sie die korrekten Daten mit ${beer.bsp} aus dem Objekt ein überall wo <!-- ToDo: ... --> steht */
     const modalHtml = `
@@ -69,72 +69,74 @@ function getBeerDetails(beerID) {
             <div class="modal-dialog modal-xl">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="exampleModalLabel"><!--ToDo: Name--></h1>
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">${beer.name}</h1>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
                         <div class="row row-cols-2 g-4">
                             <div>
-                                <img src="<!--ToDo: Image-->" class="img-fluid mb-3">
+                                <img src="${beer.image_url}" class="img-fluid mb-3">
                                 <div class="d-flex justify-content-center">
                                     <div class="px-2">
-                                        <p><b>ingredients: malt</b></p>
+                                        <p><b>Ingredients: Malt</b></p>
                                         <ul>
-                                           <!-- ToDo: malt name => Hinweis map! Ausgabe in <li> nicht vergessen -->
+                                           ${beer.ingredients.malt.map(malt => `<li> ${malt.name}</li>`).join('')}<!-- ToDo: malt name => Hinweis map! Ausgabe in <li> nicht vergessen -->
                                         </ul>
                                     </div>
                                     <div class="px-2">
-                                        <p><b>ingredients: hops</b></p>
+                                        <p><b>Ingredients: Hops</b></p>
                                         <ul>
-                                           <!-- ToDo: hops name => Hinweis map! Ausgabe in <li> nicht vergessen -->
+                                           ${beer.ingredients.hops.map(hops => `<li> ${hops.name}</li>`).join('')}<!-- ToDo: hops name => Hinweis map! Ausgabe in <li> nicht vergessen -->
                                         </ul>
                                     </div>
                                 </div>
                             </div>
                             <div>
                                 <table class="table">
-                                    <thead>
-                                    <tr>
-                                        <th scope="col">#</th>
-                                        <th scope="col">#</th>
-                                    </tr>
-                                    </thead>
+                                    
                                     <tbody>
                                     <tr>
-                                        <td>first brewed</td>
-                                        <td><!--ToDo: first brew--></td>
+                                        <td>First Brewed</td>
+                                        <td>${beer.first_brewed}</td>
                                     </tr>
                                     <tr>
-                                        <td>Volumne</td>
-                                        <td><!--ToDo: Volumne Value--> <!--ToDo: Unit--></td>
+                                        <td>Volume</td>
+                                        <td>${beer.volume.value} ${beer.volume.unit}</td>
                                     </tr>
                                     <tr>
-                                        <td>boil volume</td>
-                                        <td><!--ToDo: Boil Value--> <!--ToDo: Boil Unit--></td>
+                                        <td>Boil Volume</td>
+                                        <td>${beer.boil_volume.value} ${beer.boil_volume.unit}</td>
                                     </tr>
         
                                     <tr>
-                                        <td>methods</td>
-                                        <td><!--ToDo: Methods Mash Temp value--> <!--ToDo: Methods Mash Temp Unit--></td>
+                                        <td>Mash Temperatures</td>
+                                        <td>
+                                            <ul>
+                                                ${beer.method.mash_temp.map(mash_temp => `<li>${mash_temp.temp.value} ${mash_temp.temp.unit} for ${mash_temp.duration ?? "unknown"} minutes</li>`).join('') }
+                                            </ul>
+                                        </td>
                                     </tr>
                                     <tr>
-                                        <td>fermentation</td>
-                                        <td><!--ToDo: Methods fermentation Temp value--> <!--ToDo: Methods fermentation Temp Unit--></td>
+                                        <td>Fermentation</td>
+                                        <td>${beer.method.fermentation.temp.value} ${beer.method.fermentation.temp.unit}<!--ToDo: Methods fermentation Temp value--> <!--ToDo: Methods fermentation Temp Unit--></td>
                                     </tr>
                                     </tbody>
                                 </table>
                                 <p><b>Food Pairing</b></p>
                                 <ul>
-                                    <!--ToDo: food pairing => Hinweis map! Ausgabe in <li> nicht vergessen-->
+                                   ${beer.food_pairing.map(pairing => `<li>${pairing}</li>`).join('')} <!--ToDo: food pairing => Hinweis map! Ausgabe in <li> nicht vergessen-->
                                 </ul>
                             </div>
                         </div>
                         <p>
-                        <!--ToDo: brewers tip-->
+                        <b>Description: </b> ${beer.description}
+                        </p>
+                        <p>
+                        <b>Brewer's Tip: </b> ${beer.brewers_tips}
                         </p>
                     </div>
                     <div class="modal-footer">
-                        <p><small><!--ToDo: contributed by--></small></p>
+                        <p><small>${beer.contributed_by}</small></p>
                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                     </div>
                 </div>
